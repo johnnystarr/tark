8dc107e3ab1c26180c6687cff3ea594983d51c28
Johnny Starr <johnny.starr@appdynamics.com>
2022-03-22 17:40:06 -0500

    working solution: flyway, exposed, postgresql

--- app/src/main/kotlin/io/johnnystarr/tark/App.kt
+++ app/src/main/kotlin/io/johnnystarr/tark/App.kt
@@ -1,5 +1,14 @@
 package io.johnnystarr.tark
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
 class App {
     val bootMessage: String = "TARK version ${Config.VERSION}"
 }
@@ -7,4 +16,29 @@ class App {
 fun main() {
     Terminal.write(Config.SPLASH)
     Terminal.write("{w${App().bootMessage}")
+    initDatabase()
+
+    transaction {
+        Users.deleteAll()
+
+        Users.insert {
+            it[name] = "Johnny"
+            it[age] = 1
+        }
+
+        Users.selectAll().forEach {
+            println(it[Users.name])
+            println("Age is " + it[Users.age])
+        }
+    }
+}
+
+fun initDatabase(){
+    val workingDir = System.getProperty("user.dir")
+    val hikariConfig = HikariConfig("${workingDir}/../db.properties")
+    val dataSource = HikariDataSource(hikariConfig)
+
+    val flyway = Flyway.configure().dataSource(dataSource).load()
+    flyway.migrate()
+    Database.connect(dataSource)
 }
