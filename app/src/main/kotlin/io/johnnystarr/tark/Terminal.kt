484074531fbdaa37ed87d52c1b88a670b3306f81
Johnny Starr <johnny.starr@appdynamics.com>
2022-03-22 11:51:52 -0500

    refactor: using Terminal object

--- app/src/main/kotlin/io/johnnystarr/tark/Terminal.kt
+++ app/src/main/kotlin/io/johnnystarr/tark/Terminal.kt
@@ -3,7 +3,35 @@ package io.johnnystarr.tark
 class Terminal {
     companion object {
         fun write(str: String) {
-            println(Util.colorize(str))
+            println(colorize(str))
+        }
+
+        fun colorize(str: String) : String {
+            var colorized: String = str
+            val colors: Map<String, String> = mapOf(
+                "{x" to "\u001b[0;30m",
+                "{r" to "\u001b[0;31m",
+                "{g" to "\u001b[0;32m",
+                "{y" to "\u001b[0;33m",
+                "{b" to "\u001b[0;34m",
+                "{m" to "\u001b[0;35m",
+                "{c" to "\u001b[0;36m",
+                "{w" to "\u001b[0;37m",
+                "{X" to "\u001b[1;30m",
+                "{R" to "\u001b[1;31m",
+                "{G" to "\u001b[1;32m",
+                "{Y" to "\u001b[1;33m",
+                "{B" to "\u001b[1;34m",
+                "{M" to "\u001b[1;35m",
+                "{C" to "\u001b[1;36m",
+                "{W" to "\u001b[1;37m"
+            )
+
+            for (color in colors) {
+                colorized = colorized.replace(color.key, color.value)
+            }
+
+            return colorized
         }
     }
 }
